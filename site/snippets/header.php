3454316316131ff3c5ec2fca084272a5b403d728
ash <zhou@hm.edu>
2026-04-15 17:00:28 +0200

    icons updated for browser

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,77 +3,79 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <title>
-    <?= $site->title()->esc()?> |
-    <?= $page->title()->esc()?>
-  </title>
-  <?php
-/*
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>
+        <?= $site->title()->esc() ?> |
+        <?= $page->title()->esc() ?>
+    </title>
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <?php
+    /*
  Stylesheets can be included using the `css()` helper.
  Kirby also provides the `js()` helper to include script file.
  More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers
  */
-?>
-  <?= css([
-  'assets/css/prism.css',
-  'assets/css/lightbox.css',
-  'assets/css/index.css', // Hier sind die UntitledSans @font-face Regeln drin
-  'assets/css/templates/header.css',
-  'assets/css/templates/' . $page->template() . '.css'
-])?>
+    ?>
+    <?= css([
+        'assets/css/prism.css',
+        'assets/css/lightbox.css',
+        'assets/css/index.css', // Hier sind die UntitledSans @font-face Regeln drin
+        'assets/css/templates/header.css',
+        'assets/css/templates/' . $page->template() . '.css'
+    ]) ?>
 
-  <?php
-  /*
+    <?php
+    /*
     The `url()` helper is a great way to create reliable
     absolute URLs in Kirby that always start with the
     base URL of your site.
   */
-  ?>
-  <link rel="shortcut icon" type="image/svg" href="<?= url('assets\icons\HM_Logo_rot_cube_RGB.svg') ?>">
+    ?>
+    <link rel="shortcut icon" type="image/svg" href="<?= url('assets\icons\HM_Logo_rot_cube_RGB.svg') ?>">
 </head>
 <header class="header">
-  <div class="header_container">
-    <a class="logo" href="https://hm.edu/">
-      <img src="<?= url('assets/icons/HM_Logo_rot_cube_RGB.svg')?>" alt="Hochschule München" />
-    </a>
-    <button class="menu-toggle">
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-    <nav class="menu">
-      <ul>
-        <li>
-          <a class="<?= $page->isHomePage() ? 'current' : ''?>" href="<?= $site->url()?>">Über das Buch</a>
-        </li>
-        <?php foreach ($site->children()->listed() as $item): ?>
-        <li>
-          <a class="<?= $item->isOpen() ? 'current' : ''?>" href="<?= $item->url()?>">
-            <?php if ($item->uid() === 'projekte'): ?>
-            Studentische Projekte
-            <?php
-  else: ?>
-            <?= $item->title()->esc()?>
-            <?php
-  endif ?>
-          </a>
-        </li>
-        <?php
-endforeach ?>
-      </ul>
-    </nav>
-  </div>
+    <div class="header_container">
+        <a class="logo" href="https://hm.edu/">
+            <img src="<?= url('assets/icons/HM_Logo_rot_cube_RGB.svg') ?>" alt="Hochschule München" />
+        </a>
+        <button class="menu-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a class="<?= $page->isHomePage() ? 'current' : '' ?>" href="<?= $site->url() ?>">Über das Buch</a>
+                </li>
+                <?php foreach ($site->children()->listed() as $item): ?>
+                    <li>
+                        <a class="<?= $item->isOpen() ? 'current' : '' ?>" href="<?= $item->url() ?>">
+                            <?php if ($item->uid() === 'projekte'): ?>
+                                Studentische Projekte
+                            <?php
+                            else: ?>
+                                <?= $item->title()->esc() ?>
+                            <?php
+                            endif ?>
+                        </a>
+                    </li>
+                <?php
+                endforeach ?>
+            </ul>
+        </nav>
+    </div>
 </header>
 
 <script>
-  const menuButton = document.querySelector(".menu-toggle");
-  const menuList = document.querySelector('.menu ul');
+    const menuButton = document.querySelector(".menu-toggle");
+    const menuList = document.querySelector('.menu ul');
 
-  menuButton.addEventListener('click', () => {
-    menuList.classList.toggle('sichtbar');
-    menuButton.classList.toggle('active');
-  });
-
+    menuButton.addEventListener('click', () => {
+        menuList.classList.toggle('sichtbar');
+        menuButton.classList.toggle('active');
+    });
 </script>
